38564bfddf07c5ecf6ecf9c2af2331726289d4b6
Wush Wu <wush978@gmail.com>
2018-12-02 22:10:25 +0800

    update

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
-all :
-	Rscript R/prebuild.R
-	Rscript -e "blogdown::build_site()"
+SOURCE_RMD=$(wildcard source/post/*.Rmd)
+CONTENT_RMD=$(shell echo ${SOURCE_RMD} | tr " " "\n" | sed "s/^source/content/g")
+CONTENT_HTML=$(CONTENT_RMD:.Rmd=.html)
+
+.PHONY=hugo install
+
+all : hugo
+
+install :
+	Rscript -e "if (!require('pvm')) install.packages('pvm', repos = 'https://wush978.github.io/R', type = 'source')"
+	Rscript -e "pvm::import.packages()"
+	Rscript -e "blogdown::install_hugo()"
+	
+content/post/%.Rmd : source/post/%.Rmd
+	Rscript R/source2content.R $< $@
+
+content/post/%.html : content/post/%.Rmd
+	Rscript -e "knitr::knit('$<', '$@')"
+
+hugo : $(CONTENT_HTML)
+	Rscript -e "blogdown::hugo_build()"
+
---
+++ R/source2content.R
@@ -0,0 +1,27 @@
+library(methods)
+library(magrittr)
+
+argv <- commandArgs(TRUE)
+.f <- file(path <- argv[1], encoding = "UTF-8")
+src <- readLines(.f)
+close(.f)
+stopifnot(sum(src == "---") == 2)
+. <- which(src == "---")
+start <- .[1] + 1
+end <- .[2] - 1
+src[seq.int(start, end, by = 1)]
+.config <- yaml::yaml.load(src[seq.int(start, end, by = 1)])
+.content <- src[seq.int(end + 2, length(src), by = 1)]
+.config$date <- strptime(.config$date, "%Y-%m-%d %H:%M") %>%
+  as.Date() %>%
+  format()
+.config$draft <- FALSE
+.config$slug <- substring(basename(path), 12, nchar(basename(path))) %>%
+  tools::file_path_sans_ext()
+.config <- .config[c("title", "date", "slug")]
+.f <- file(argv[2], open = "w", encoding = "UTF-8")
+c(sprintf("---\n%s---\n", yaml::as.yaml(.config)), .content) %>%
+  cat(file = .f, sep = "\n")
+cat("\n", file = .f, append = TRUE)
+close(.f)
+
